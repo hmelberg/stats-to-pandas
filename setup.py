b77d748ee12fa843bf1d51f71c6ab49281aa4d48
Hans Olav Melberg <hans.melberg@gmail.com>
2016-06-16 21:41:07 +0200

    hyphen to underscore, new version 0.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'stats-to-pandas',
-  packages = ['stats-to-pandas'], # this must be the same as the name above
-  version = '0.0.1',
+  name = 'stats_to_pandas',
+  packages = ['stats_to_pandas'], 
+  version = '0.0.2',
   description = 'Import data from Statistics Norway, Sweden, Ireland and the UK to a Pandas dataframe in Python',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
-  url = 'https://github.com/hmelberg/stats-to-pandas', # use the URL to the github repo
-  download_url = 'https://github.com/hmelberg/stats-to-pandas/tarball/0.0.1', # I'll explain this in a second
-  keywords = ['pandas', 'Statistics Norway', 'json-stat'], # arbitrary keywords
+  url = 'https://github.com/hmelberg/stats_to_pandas', # use the URL to the github repo
+  download_url = 'https://github.com/hmelberg/stats_to_pandas/tarball/0.0.2', 
+  keywords = ['pandas', 'Statistics Norway', 'json-stat'], 
   classifiers = [],
 )
