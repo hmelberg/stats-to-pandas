c6ec91a69e8acc6d5be7c2f0bcfe7970583336b4
Hans Olav Melberg <hans.melberg@gmail.com>
2016-06-17 02:45:17 +0200

    removed automatic installation of dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 setup(
   name = 'stats_to_pandas',
   packages = ['stats_to_pandas'], 
-  version = '0.0.6',
+  version = '0.0.7',
   description = 'Import data from Statistics Norway, Sweden, Ireland , UK and others that use the stat-json format to a Pandas dataframe in Python',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
   url = 'https://github.com/hmelberg/stats-to-pandas', # use the URL to the github repo
-  download_url = 'https://github.com/hmelberg/stats-to-pandas/tarball/0.0.6', 
+  download_url = 'https://github.com/hmelberg/stats-to-pandas/tarball/0.0.7', 
   keywords = ['pandas', 'Statistics Norway', 'json-stat'], 
   classifiers = [],
-  install_requires=['requests', 'pyjstat'],
 )
