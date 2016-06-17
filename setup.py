4a44a9d151db3f986e0baa00b6705d0a0243536b
Hans Olav Melberg <hans.melberg@gmail.com>
2016-06-17 02:32:45 +0200

    name change to init and included two dependencies
    put functions in init file to avoid having to repeat name when
    importing.
    included requests and pyjstat as dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'stats_to_pandas',
   packages = ['stats_to_pandas'], 
-  version = '0.0.5',
-  description = 'Import data from Statistics Norway, Sweden, Ireland and the UK to a Pandas dataframe in Python',
+  version = '0.0.6',
+  description = 'Import data from Statistics Norway, Sweden, Ireland , UK and others that use the stat-json format to a Pandas dataframe in Python',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
   url = 'https://github.com/hmelberg/stats-to-pandas', # use the URL to the github repo
-  download_url = 'https://github.com/hmelberg/stats-to-pandas/tarball/0.0.5', 
+  download_url = 'https://github.com/hmelberg/stats-to-pandas/tarball/0.0.6', 
   keywords = ['pandas', 'Statistics Norway', 'json-stat'], 
   classifiers = [],
+  install_requires=['requests', 'pyjstat'],
 )
